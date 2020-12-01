1b2962abfca4259f251ed12665b6ee5bb016d819
Saurav Kumar <sauravk7077@gmail.com>
2020-12-01 09:09:27 +0530

    added basic code

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/choose_location.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
